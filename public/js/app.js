4e846f9b4edc467e14b0c3776c6d512052afa498
kiis-2182517 <levietnam1998@gmail.com>
2022-09-04 10:23:33 +0700

    commit2

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log('Client side js fire')
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
